ef3b83a6d52787de8d85d531e7713d6fe5792fdb
Ethen <thay279931@gmail.com>
2022-10-21 14:54:03 +0800

    ws: echo server

--- routes/ws-echo.js
+++ routes/ws-echo.js
@@ -4,7 +4,7 @@ const createEchoServer = server => {
     const wsServer = new WebSocket.Server({server});
 
     wsServer.on('connection', (ws, req)=>{
-        ws.send('-----');
+        ws.send('-----連線數'+wsServer.clients.size);
         ws.on('message', message=>{
             ws.send(message.toString());
         });
